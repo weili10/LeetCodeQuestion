58782d79e57af98e72bf16511e49d5791e74b690
weili10 <lwei1993@gmail.com>
2018-04-01 00:36:45 +1100

    Remove Duplicates from Sorted List

---
+++ problem83.py
@@ -0,0 +1,31 @@
+'''
+83. Remove Duplicates from Sorted List
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        temp = head
+        if temp == None:
+            return None
+        while temp.next:
+            if temp.val == temp.next.val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        return head
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from problem69 import Solution
+from problem83 import Solution
 
 class ListNode(object):
     def __init__(self, x):
@@ -8,11 +8,17 @@ class ListNode(object):
 l1 = ListNode(5)
 
 l2 = ListNode(1)
-l2.next = ListNode(2)
-l2.next.next = ListNode(4)
-
+l2.next = ListNode(1)
+l2.next.next = ListNode(2)
+l2.next.next.next = ListNode(3)
+l2.next.next.next.next = ListNode(3)
 sol = Solution()
 
 # print(sol.removeElement([2,2,3],2))
-print(sol.mySqrt(4))
+# print(sol.deleteDuplicates(l2))
+
+l = sol.deleteDuplicates(l2)
+while l:
+    print(l.val)
+    l = l.next
 
